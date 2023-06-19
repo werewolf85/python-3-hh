a2e15bb4d68e2caf7424fba8f5635e02f9de5734
werewolf85 <ixbox85@gmail.com>
2023-06-19 21:10:28 +0600

    first commit

---
+++ core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def homepage(request):
+    return HttpResponse("hi")
+
+def about(request):
+    return HttpResponse("Найдите работу или работника мечты")
+
+def contact_view(request):
+    return HttpResponse('''
+    <div>
+    Phone: +996777123456 </br>
+    Email: office@handhunter.kg
+    </div>
+    ''')
+
+def address(request):
+    return HttpResponse('''
+    <div>
+        <ol>
+            <li>Address: street Chui, 777</li>
+            <li>Address: 7 MKR, 20</li>
+            <li>Address: 12 MKR, street Nurkamal Jeticashkaeva, 45</li>
+        </ol>
+    </div>
+    ''')
